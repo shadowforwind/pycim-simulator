b055bc5a4f3892d2ce584dd64cbf3ecbb0416031
shadowforwind <peixiangli@quanta.org.cn>
2024-12-24 17:14:52 +0800

    release/0.0.2

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="pycim-simulator",
+    version="0.0.1",
+    author="peixiangli",
+    author_email="peixiangli@quanta.org.cn",
+    description="A Python Framework for the Dynamics of Coherent Ising Machine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shadowforwind/pycim-simulator",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7.7",
+)
